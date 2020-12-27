f834442f81920d92759e67f0a26459cf83078e9c
ryonalish <61942576+ryonalish@users.noreply.github.com>
2020-12-27 21:29:00 +0800

    Update aqi-us.js

--- aqi-us.js
+++ aqi-us.js
@@ -1,5 +1,5 @@
 
-	[rewrite-remote]
+	[Scripts]
 	https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include= url script-response-body https://github.com/ryonalish/surge/raw/main/aqi-us.js
 
 	[MITM]
